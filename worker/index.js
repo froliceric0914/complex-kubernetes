68850a63a60fbe20cf2563585400c7adad29e713
froliceric0914 <wzhao.eric@gmail.com>
2020-12-29 17:08:07 -0500

    add Dockerfile.dev in all folders

--- worker/index.js
+++ worker/index.js
@@ -9,7 +9,10 @@ const redisClient =redis.createClient({
 
 const sub = redisClient.duplicate();
 
-const fib = (index) => index < 2 ? 1 : (fib(index - 1 ) + fib(index -2));
+function fib(index) {
+    if (index < 2) return 1;
+    return fib(index - 1) + fib(index - 2);
+}
 
 sub.on('message', (channel, message) =>{
     redisClient.hset('values', message, fib(parseInt(message)))
